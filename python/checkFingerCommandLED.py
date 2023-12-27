7f39665dd8fb496951a170d21d4adbe3aa8fad71
mannaja <manman7852078520@gmail.com>
2023-12-27 16:42:50 +0700

    lab complete

--- python/checkFingerCommandLED.py
+++ python/checkFingerCommandLED.py
@@ -11,7 +11,7 @@ mp_draw=mp.solutions.drawing_utils #use function drawing_utils to draw straight 
 mp_hand=mp.solutions.hands #use function hands to find hand on camera
 
 
-tipIds=[4,8,12,16,20] # media-pipe position  of fingertips
+tipIds = [4, 8, 12, 16, 20] # media-pipe position  of fingertips
 
 def check_user_input(input):
     try:
@@ -39,8 +39,10 @@ comport = input('Enter the arduino board COM port: ')
 while not (check_user_input(comport)):
     print('Please enter a number not string')
     comport = input('Enter the arduino board COM port: ')
+    # comport = input('Enter the arduino board USB port: ')
 
-board=pyfirmata.Arduino('COM'+comport)
+# board=pyfirmata.Arduino('COM'+comport) #window
+board=pyfirmata.Arduino('/dev/ttyUSB'+comport) #Linux
 led_1=board.get_pin('d:13:o') #Set pin to output
 led_2=board.get_pin('d:12:o')
 led_3=board.get_pin('d:11:o')
@@ -51,46 +53,14 @@ led_5=board.get_pin('d:9:o')
 
 ## controller.py ##
 
-def led(total,led_1,led_2,led_3,led_4,led_5):#creat condition to controll digital out put
-    if total==0:
-        led_1.write(0)
-        led_2.write(0)
-        led_3.write(0)
-        led_4.write(0)
-        led_5.write(0)
-    elif total==1:
-        led_1.write(1)
-        led_2.write(0)
-        led_3.write(0)
-        led_4.write(0)
-        led_5.write(0)
-    elif total==2:
-        led_1.write(1)
-        led_2.write(1)
-        led_3.write(0)
-        led_4.write(0)
-        led_5.write(0)
-    elif total==3:
-        led_1.write(1)
-        led_2.write(1)
-        led_3.write(1)
-        led_4.write(0)
-        led_5.write(0)
-    elif total==4:
-        led_1.write(1)
-        led_2.write(1)
-        led_3.write(1)
-        led_4.write(1)
-        led_5.write(0)
-    elif total==5:
-        led_1.write(1)
-        led_2.write(1)
-        led_3.write(1)
-        led_4.write(1)
-        led_5.write(1)
-
-
-
+def led(ledCommand,led_1,led_2,led_3,led_4,led_5):#creat condition to controll digital out put
+    
+    led_1.write(ledCommand[0])
+    led_2.write(ledCommand[1])
+    led_3.write(ledCommand[2])
+    led_4.write(ledCommand[3])
+    led_5.write(ledCommand[4])
+    
 ######
 
 
@@ -107,6 +77,8 @@ with mp_hand.Hands(min_detection_confidence=0.5,
         image.flags.writeable=True #can drawing  image
         image=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  #convert color RGB to BGR
         lmList=[]
+        ledCommand  = [0, 0, 0, 0, 0]
+        Hands = 0
         if results.multi_hand_landmarks:
             for hand_landmark in results.multi_hand_landmarks:
                 myHands=results.multi_hand_landmarks[0]
@@ -115,17 +87,21 @@ with mp_hand.Hands(min_detection_confidence=0.5,
                     cx,cy= int(lm.x*w), int(lm.y*h)
                     lmList.append([id,cx,cy])  #input number hand_landmark position and position of spot position hand_landmark
                 mp_draw.draw_landmarks(image, hand_landmark, mp_hand.HAND_CONNECTIONS)  #drawing hand skeleton from hand_landmark point
+                Hands = Hands + 1
         fingers=[]
         if len(lmList)!=0:
             #Thumb check (LHS and RHS conditions) (x-position)
             if lmList[9][1]<lmList[5][1]:
                 if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
                     fingers.append(1)
+                    ledCommand[0] = 1
+                    
                 else:
                     fingers.append(0)
             else:
                 if lmList[tipIds[0]][1] < lmList[tipIds[0]-1][1]:
                     fingers.append(1)
+                    ledCommand[0] = 1
                 else:
                     fingers.append(0)
             #Other finger check (y-position)
@@ -134,17 +110,20 @@ with mp_hand.Hands(min_detection_confidence=0.5,
                 if lmList[0][2]<lmList[5][2]: #creat condition for count fingers
                     if lmList[tipIds[id]][2] > lmList[tipIds[id]-2][2]:
                         fingers.append(1)
+                        ledCommand[id] = 1
                     else:
                         fingers.append(0)
                 # Upside down
                 else:
                     if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
                         fingers.append(1)
+                        ledCommand[id] = 1
                     else:
                         fingers.append(0)
             total=fingers.count(1)
 
-            led(total,led_1,led_2,led_3,led_4,led_5) #import function in module to control arduino output
+            led(ledCommand,led_1,led_2,led_3,led_4,led_5) #import function in module to control arduino output
+            print(Hands)
             """
             creat condition to put text in frame
 
